9a9342befa35494317cb569b88df07b56afe6e66
kjh <kjh920407@naver.com>
2022-12-01 19:18:03 +0900

    add: 최초 커밋

---
+++ src/main/java/stable/horseCafe/HorseCafeApplication.java
@@ -0,0 +1,13 @@
+package stable.horseCafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorseCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorseCafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/stable/horseCafe/HorseCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package stable.horseCafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorseCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
