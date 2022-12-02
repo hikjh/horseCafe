b1839e0ee4266e67dce6576b88bd83d1259fcf2e
kjh <kjh920407@naver.com>
2022-12-02 19:13:47 +0900

    test: 메뉴 단건 조회 테스트

--- src/test/java/stable/horseCafe/service/menu/MenuServiceTest.java
+++ src/test/java/stable/horseCafe/service/menu/MenuServiceTest.java
@@ -4,17 +4,22 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import stable.horseCafe.domain.member.Member;
+import stable.horseCafe.domain.member.MemberRepository;
 import stable.horseCafe.domain.menu.Menu;
 import stable.horseCafe.domain.menu.MenuRepository;
 import stable.horseCafe.domain.menu.MenuStatus;
 import stable.horseCafe.domain.menu.MenuType;
+import stable.horseCafe.domain.review.Review;
+import stable.horseCafe.domain.review.ReviewRepository;
 import stable.horseCafe.service.member.MemberService;
+import stable.horseCafe.web.dto.menu.MenuResDto;
 import stable.horseCafe.web.dto.menu.MenuSaveReqDto;
 import stable.horseCafe.web.dto.menu.MenuUpdateReqDto;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@Transactional
+
 @SpringBootTest
 class MenuServiceTest {
 
@@ -22,6 +27,10 @@ class MenuServiceTest {
     MenuService menuService;
     @Autowired
     MenuRepository menuRepository;
+    @Autowired
+    ReviewRepository reviewRepository;
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     void 메뉴_등록_수정() {
@@ -66,5 +75,36 @@ class MenuServiceTest {
         Long result = menuService.deleteMenu(savedId);
 
         assertEquals(result, savedId);
+    }
+
+    @Transactional
+    @Test
+    void 메뉴_단건조회() {
+
+        Member member = memberRepository.save(Member.builder()
+                .name("홍길동").build());
+
+
+        MenuSaveReqDto dto = MenuSaveReqDto.builder()
+                .name("아메리카노")
+                .price(4500)
+                .stockQuantity(50)
+                .menuType(MenuType.COFFEE)
+                .menuStatus(MenuStatus.ICE)
+                .build();
+
+        Long savedId = menuService.registerMenu(dto);
+        Menu menu = menuRepository.findById(savedId).get();
+
+        Review review = reviewRepository.save(Review.builder()
+                .menu(menu)
+                .member(member)
+                .content("가나다라")
+                .build());
+
+
+        System.out.println("review = " + review);
+        MenuResDto resDto = menuService.getMenu(savedId);
+        System.out.println("resDto = " + resDto.getReviews().get(0));
     }
 }
