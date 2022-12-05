74539885c452505bf853dbb1829c90c1e1c9c186
kjh <kjh920407@naver.com>
2022-12-05 22:26:01 +0900

    fixed: ROLE key값 수정
    issue:
    forbidden 403 에러 발생.
    
    MEMBER -> ROLE_MEMBER 변경하여 해결.

--- src/main/java/stable/horseCafe/domain/member/Role.java
+++ src/main/java/stable/horseCafe/domain/member/Role.java
@@ -6,8 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Role {
-    MEMBER("MEMBER", "사용자"),
-    ADMIN("ADMIN", "관리자");
+    MEMBER("ROLE_MEMBER", "사용자"),
+    ADMIN("ROLE_ADMIN", "관리자");
 
     private final String key;
     private final String value;
