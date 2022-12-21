ec2d3d7a8854bc9b31b000ad235ecbcce7a3d692
kjh <kjh920407@naver.com>
2022-12-21 18:32:45 +0900

    add: 리뷰 등록 추가

--- src/main/java/stable/horseCafe/web/dto/review/ReviewSaveReqDto.java
+++ src/main/java/stable/horseCafe/web/dto/review/ReviewSaveReqDto.java
@@ -2,30 +2,16 @@ package stable.horseCafe.web.dto.review;
 
 import lombok.Builder;
 import lombok.Getter;
-import stable.horseCafe.domain.member.Member;
-import stable.horseCafe.domain.menu.Menu;
-import stable.horseCafe.domain.review.Review;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class ReviewSaveReqDto {
-/*
-    private Member member;
-    private Long menuId;
+
     private String content;
 
     @Builder
-    public ReviewSaveReqDto(Member member, Long menuId, String content) {
-        this.member = member;
-        this.menuId = menuId;
+    public ReviewSaveReqDto(String content) {
         this.content = content;
     }
-
-    public Review toEntity() {
-        return Review.builder()
-                .member(member)
-                .menu(menu)
-                .content(content)
-                .build();
-    }
- */
 }
