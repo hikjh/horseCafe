d697d7349896c30b664fbfe26e7feccc6dbfe09d
kjh <kjh920407@naver.com>
2022-12-27 21:01:48 +0900

    add: @Valid 에러 전역예외처리

--- src/main/java/stable/horseCafe/web/common/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/stable/horseCafe/web/common/exception/handler/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package stable.horseCafe.web.common.exception.handler;
 
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import stable.horseCafe.web.common.exception.GlobalException;
 import stable.horseCafe.web.common.response.CommonResponse;
+import stable.horseCafe.web.common.response.code.ResponseCode;
 import stable.horseCafe.web.common.util.ResponseUtil;
 
 @RestControllerAdvice
@@ -12,5 +14,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(GlobalException.class)
     public CommonResponse handle(GlobalException ge) {
         return ResponseUtil.failResponse(ge);
+    }
+
+    /**
+     *  필수값 검증 - @Valid
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public CommonResponse valid(MethodArgumentNotValidException manve) {
+        return ResponseUtil.failResponse(ResponseCode.BAD_REQUEST, manve.getBindingResult().getAllErrors().get(0).getDefaultMessage());
     }
 }
--- src/main/java/stable/horseCafe/web/controller/member/MemberController.java
+++ src/main/java/stable/horseCafe/web/controller/member/MemberController.java
@@ -1,22 +1,16 @@
 package stable.horseCafe.web.controller.member;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import stable.horseCafe.service.member.MemberService;
-import stable.horseCafe.web.common.exception.GlobalException;
 import stable.horseCafe.web.common.response.CommonResponse;
-import stable.horseCafe.web.common.response.code.ResponseCode;
 import stable.horseCafe.web.common.util.ResponseUtil;
 import stable.horseCafe.web.dto.member.MemberLoginReqDto;
 import stable.horseCafe.web.dto.member.MemberSaveReqDto;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,11 +22,7 @@ public class MemberController {
      *  회원 가입
      */
     @PostMapping("/stable/v1/signUp")
-    public CommonResponse singUp(@RequestBody @Valid MemberSaveReqDto dto, BindingResult result) {
-        if (result.hasErrors()) {
-            FieldError fieldError = result.getFieldError();
-            throw new GlobalException(ResponseCode.BAD_REQUEST, fieldError.getDefaultMessage());
-        }
+    public CommonResponse singUp(@RequestBody @Valid MemberSaveReqDto dto) {
         return ResponseUtil.getSingleResult("회원가입", memberService.signUp(dto));
     }
 
@@ -40,7 +30,7 @@ public class MemberController {
      *  로그인
      */
     @PostMapping("/stable/v1/login")
-    public CommonResponse login(@RequestBody MemberLoginReqDto dto) {
+    public CommonResponse login(@RequestBody @Valid MemberLoginReqDto dto) {
         return ResponseUtil.getSingleResult("로그인", memberService.login(dto));
     }
 }
