8e42d55da79a7ec356d2ee3b435618e5037107ab
kjh <kjh920407@naver.com>
2023-01-02 15:58:30 +0900

    test: MemberService 로그인 및 토큰 발급 테스트

--- src/test/java/stable/horseCafe/service/member/MemberServiceTest.java
+++ src/test/java/stable/horseCafe/service/member/MemberServiceTest.java
@@ -1,11 +1,15 @@
 package stable.horseCafe.service.member;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import stable.horseCafe.domain.member.Member;
 import stable.horseCafe.domain.member.MemberRepository;
+import stable.horseCafe.web.dto.member.MemberLoginReqDto;
+import stable.horseCafe.web.dto.member.MemberLoginResDto;
 import stable.horseCafe.web.dto.member.MemberSaveReqDto;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,18 +23,45 @@ class MemberServiceTest {
     @Autowired
     MemberRepository memberRepository;
 
+    @BeforeEach
+    void clean() {
+        memberRepository.deleteAll();
+    }
+
     @Test
+    @DisplayName("회원가입 및 비밀번호 암호화")
     void signUp() {
+        // given
         MemberSaveReqDto dto = MemberSaveReqDto.builder()
                                                 .name("userA")
                                                 .email("AA@naver.com")
                                                 .password("1234")
                                                 .build();
 
+        // when
         Long savedId = memberService.signUp(dto);
         Member member = memberRepository.findById(savedId).get();
 
+        // then
         assertNotNull(savedId);
-        assertEquals(member.getEmail(), dto.getEmail());
+        assertEquals(dto.getEmail(), member.getEmail());
+        assertNotEquals("1234", member.getPassword());
+    }
+
+    @Test
+    @DisplayName("로그인 및 AccessToken 발급")
+    void login() {
+        // given
+        signUp();
+        MemberLoginReqDto reqDto = MemberLoginReqDto.builder()
+                .email("AA@naver.com")
+                .password("1234")
+                .build();
+
+        // when
+        MemberLoginResDto resDto = memberService.login(reqDto);
+
+        //then
+        assertNotNull(resDto.getAccessToken());
     }
 }
