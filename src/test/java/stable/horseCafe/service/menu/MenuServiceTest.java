edd718297021fc3ed4c13fff835bd80508ea2baa
kjh <kjh920407@naver.com>
2023-01-02 18:46:34 +0900

    test: Menu 등록, 수정, 삭제, 조회, 검색조회 테스트

--- src/test/java/stable/horseCafe/service/menu/MenuServiceTest.java
+++ src/test/java/stable/horseCafe/service/menu/MenuServiceTest.java
@@ -1,5 +1,7 @@
 package stable.horseCafe.service.menu;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,12 +14,17 @@ import stable.horseCafe.domain.menu.MenuStatus;
 import stable.horseCafe.domain.menu.MenuType;
 import stable.horseCafe.web.dto.menu.MenuResDto;
 import stable.horseCafe.web.dto.menu.MenuSaveReqDto;
+import stable.horseCafe.web.dto.menu.MenuSearchCondition;
 import stable.horseCafe.web.dto.menu.MenuUpdateReqDto;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
+@Transactional
 class MenuServiceTest {
 
     @Autowired
@@ -27,9 +34,15 @@ class MenuServiceTest {
     @Autowired
     MemberRepository memberRepository;
 
-    @Test
-    void 메뉴_등록_수정() {
+    @BeforeEach
+    void clean() {
+        menuRepository.deleteAll();
+    }
 
+    @Test
+    @DisplayName("메뉴 등록")
+    void registerMenu() {
+        // given
         MenuSaveReqDto dto = MenuSaveReqDto.builder()
                 .name("아메리카노")
                 .price(4500)
@@ -38,61 +51,142 @@ class MenuServiceTest {
                 .menuStatus(MenuStatus.ICE)
                 .build();
 
+        // when
         Long savedId = menuService.registerMenu(dto);
+        Menu menu = menuRepository.findById(savedId).get();
 
-        MenuUpdateReqDto updateReqDto = MenuUpdateReqDto.builder()
-                .name("아메리카노22")
-                .price(5500)
-                .stockQuantity(100)
+        // then
+        assertNotNull(menu);
+        assertEquals(dto.getName(), menu.getName());
+        assertEquals(dto.getPrice(), menu.getPrice());
+        assertEquals(dto.getMenuType(), menu.getMenuType());
+        assertEquals(dto.getMenuStatus(), menu.getMenuStatus());
+    }
+
+    @Test
+    @DisplayName("메뉴 수정")
+    void editMenu() {
+        // given
+        Menu menu = Menu.builder()
+                .name("아이스 아메리카노")
+                .price(5000)
+                .stockQuantity(50)
+                .menuType(MenuType.COFFEE)
+                .menuStatus(MenuStatus.ICE)
+                .build();
+        menuRepository.save(menu);
+
+        // when
+        MenuUpdateReqDto reqDto = MenuUpdateReqDto
+                .builder()
+                .name("아메리카노")
+                .price(4500)
+                .stockQuantity(45)
                 .menuType(MenuType.COFFEE)
                 .menuStatus(MenuStatus.HOT)
                 .build();
+        menuService.editMenu(menu.getId(), reqDto);
 
-        Long updatedId = menuService.editMenu(savedId, updateReqDto);
-        Menu menu = menuRepository.findById(updatedId).get();
-
-        assertNotNull(savedId);
-        assertEquals(menu.getName(), updateReqDto.getName());
-        assertEquals(menu.getMenuType(), updateReqDto.getMenuType());
+        // then
+        assertNotNull(menu);
+        assertEquals("아메리카노", menu.getName());
+        assertEquals(4500, menu.getPrice());
+        assertEquals(45, menu.getStockQuantity());
+        assertEquals(MenuType.COFFEE, menu.getMenuType());
+        assertEquals(MenuStatus.HOT, menu.getMenuStatus());
     }
 
     @Test
-    void 메뉴_삭제() {
-        MenuSaveReqDto dto = MenuSaveReqDto.builder()
-                .name("아메리카노")
-                .price(4500)
+    @DisplayName("메뉴 삭제")
+    void deleteMenu() {
+        // given
+        Menu menu = Menu.builder()
+                .name("아이스 아메리카노")
+                .price(5000)
                 .stockQuantity(50)
                 .menuType(MenuType.COFFEE)
                 .menuStatus(MenuStatus.ICE)
                 .build();
 
-        Long savedId = menuService.registerMenu(dto);
-        Long result = menuService.deleteMenu(savedId);
+        menuRepository.save(menu);
 
-        assertEquals(result, savedId);
+        // when
+        menuService.deleteMenu(menu.getId());
+
+        // then
+        //assertNull(menu.getName());
     }
 
-    @Transactional
     @Test
-    void 메뉴_단건조회() {
-
-        memberRepository.save(Member.builder()
-                .name("홍길동").build());
-
-
-        MenuSaveReqDto dto = MenuSaveReqDto.builder()
-                .name("아메리카노")
-                .price(4500)
+    @DisplayName("메뉴 단건 조회")
+    void getMenu() {
+        // given
+        Menu menu = Menu.builder()
+                .name("아이스 아메리카노")
+                .price(5000)
                 .stockQuantity(50)
                 .menuType(MenuType.COFFEE)
                 .menuStatus(MenuStatus.ICE)
                 .build();
+        menuRepository.save(menu);
 
-        Long savedId = menuService.registerMenu(dto);
-        MenuResDto resultMenu = menuService.getMenu(savedId);
+        // when
+        MenuResDto resultMenu = menuService.getMenu(menu.getId());
 
-        assertEquals(resultMenu.getMenuStatus(), dto.getMenuStatus());
-        assertEquals(resultMenu.getMenuType(), dto.getMenuType());
-        assertEquals(resultMenu.getName(), dto.getName());
+        // then
+        assertNotNull(resultMenu);
+        assertEquals("아이스 아메리카노", resultMenu.getName());
+        assertEquals(5000, resultMenu.getPrice());
+        assertEquals(MenuType.COFFEE, resultMenu.getMenuType());
+        assertEquals(MenuStatus.ICE, resultMenu.getMenuStatus());
+    }
+
+
+    @Test
+    @DisplayName("메뉴 목록 검색 조회 - 전체, 메뉴 유형, 메뉴 상태")
+    void getMenuList() {
+        // given
+        List<Menu> menuList = saveMenuList();// 메뉴 10개 생성 - 5개: ICE, 5개: HOT
+        menuRepository.saveAll(menuList);
+
+        MenuSearchCondition condition = MenuSearchCondition.builder()
+                .menuName(null)
+                .menuType(null)
+                .menuStatus(MenuStatus.HOT)
+                .build();
+        
+        // when
+        List<MenuResDto> resultMenuList = menuService.getMenuList(condition);
+
+        // then
+        assertEquals(5, resultMenuList.size());
+        assertEquals(MenuStatus.HOT, resultMenuList.get(0).getMenuStatus());
+    }
+
+    private List<Menu> saveMenuList() {
+        List<Menu> menuList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Menu menu;
+            if (i % 2 == 0) {
+                menu = Menu.builder()
+                        .name("아이스 아메리카노" + i)
+                        .price(5000)
+                        .stockQuantity(50)
+                        .menuType(MenuType.COFFEE)
+                        .menuStatus(MenuStatus.HOT)
+                        .build();
+            } else {
+                menu = Menu.builder()
+                        .name("아이스 아메리카노" + i)
+                        .price(5000)
+                        .stockQuantity(50)
+                        .menuType(MenuType.COFFEE)
+                        .menuStatus(MenuStatus.ICE)
+                        .build();
+            }
+            menuList.add(menu);
+        }
+
+        return menuList;
     }
 }
