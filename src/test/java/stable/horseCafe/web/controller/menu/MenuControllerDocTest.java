0ab2dec50b3da4891ea16482299bd99aff3c5815
kjh <kjh920407@naver.com>
2023-01-10 19:45:39 +0900

    test: Spring REST Docs 테스트 생성 및 adoc 파일 생성

---
+++ src/test/java/stable/horseCafe/web/controller/menu/MenuControllerDocTest.java
@@ -0,0 +1,61 @@
+package stable.horseCafe.web.controller.menu;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+import stable.horseCafe.domain.menu.Menu;
+import stable.horseCafe.domain.menu.MenuRepository;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import static stable.horseCafe.domain.menu.MenuStatus.HOT;
+import static stable.horseCafe.domain.menu.MenuType.COFFEE;
+
+@SpringBootTest
+@ExtendWith(RestDocumentationExtension.class)
+public class MenuControllerDocTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private MenuRepository menuRepository;
+
+    @BeforeEach
+    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation))
+                .build();
+    }
+
+    @Test
+    @DisplayName("메뉴 단건 조회")
+    void registerMenu() throws Exception {
+        // given
+        Menu menu = Menu.builder()
+                .name("아메리카노")
+                .price(5000)
+                .stockQuantity(50)
+                .menuType(COFFEE)
+                .menuStatus(HOT).build();
+        menuRepository.save(menu);
+
+        // expected
+        this.mockMvc.perform(get("/stable/v1/menu/{menuId}", 1L)
+                        .accept(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andDo(document("index"));
+    }
+}
